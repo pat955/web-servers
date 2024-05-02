a94990a7f3dde1685563cdb44354f23cae2a1759
pat955 <patricijap8@gmail.com>
2024-05-02 13:09:49 +0200

    chirps

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -14,9 +16,10 @@ func main() {
 	}
 	mux := http.NewServeMux()
 	defaultHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(root))))
-	mux.Handle("/app/*", defaultHandler)
+	mux.Handle("/app/*", middlewareLog(defaultHandler))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCount)
 	mux.HandleFunc("GET /api/healthz", handlerStatus)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	mux.HandleFunc("/api/reset", apiCfg.handlerResetCount)
 	corsMux := middlewareCors(mux)
 
@@ -43,6 +46,31 @@ func handlerStatus(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+func respondWithJSON() {}
+func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type post struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		Valid bool `json:"valid"`
+	}
+	newPOST := post{}
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&newPOST)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(`"error":"something went wrong"`))
+		fmt.Println("POST FAILED", err)
+	}
+	if len(newPOST.Body) > 140 {
+		w.WriteHeader(400)
+		w.Write([]byte(`"error":"Chirp is too long"`))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte(`"valid":true`))
+	}
 }
 
 func middlewareLog(next http.Handler) http.Handler {
